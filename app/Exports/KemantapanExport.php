8e1cd59d90cc3acdb2a434d14b7e990e0023c792
syukron <syukron488@gmail.com>
2024-05-21 12:45:26 +0700

    fiks number format on jenis perkerasan, kemantapan, dashboard

--- app/Exports/KemantapanExport.php
+++ app/Exports/KemantapanExport.php
@@ -93,6 +93,7 @@ class KemantapanExport implements FromView
 
     public function number_format($angka)
     {
+        $angka = floatval($angka);
         $formatted_number = number_format($angka, 2);
 
         if (strpos($formatted_number, ".00") !== false) {
